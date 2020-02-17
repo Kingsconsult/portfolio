b73a05171df176f583faf5adc24cd49583568288
kingsley okpara <kingsconsult001@gmail.com>
2020-02-17 16:39:16 +0100

    I setup the react app I setup the storybook for the react app

--- src/headers.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
-import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
-
-export const header = () => (
-    
-)
