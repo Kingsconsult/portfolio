e577787de2e5194a7496ee06eef6841ae7ce4f56
kingsley okpara <kingsconsult001@gmail.com>
2020-02-17 15:42:37 +0100

    type: setup
    I setup the react app for the portfolio
    
    I setup the storybook for the react project

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links/register'
+  ],
+};
---
+++ src/headers.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
+export const header = () => (
+    
+)
