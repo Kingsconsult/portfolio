d909d368957de735f80fe57769fb6b4e0a290332
kingsley okpara <kingsconsult001@gmail.com>
2020-02-21 08:29:51 +0100

    updated the card components

--- src/components/colors/index.js
+++ src/components/colors/index.js
@@ -1,45 +1,48 @@
-import theme from 'styled-theming';
+import theme from "styled-theming";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-
-
 export const color = theme.variants("mode", "color", {
-         white: { light: "color :#FFFFFF" },
-         whiteDark: { light: "color :#F9F9FB" },
-         whiteDarker: { light: "color :#F6F7FB" },
-         blue: { light: "color :#5569FF" },
-         blueLight: { light: "color :#4355FE" },
-         blueGrey: {light: "color :#EEF0FC" },
-         blueGreyDark: { light: "color :#C2C9FD" },
-         blueGreyLight: { light: "color :#EDF1fc" },
-         black: { light: "color :#353535" },
-         blackDeep: { light: "color :#1D1D1D" },
-         blackLight: { light: "color :#FEFEFE" },
-       });
-  
-  export const bgColor = theme.variants("mode", "bgColor", {
-    white: { light: "background-color :#FFFFFF" },
-    whiteDark: { light: "background-color :#F9F9FB" },
-    whiteDarker: { light: "background-color :#F6F7FB" },
-    blue : { light: "background-color :#5569FF" },
-    blueLight: { light: "background-color :#4355FE" },
-    blueGrey: {light: "background-color :#EEF0FC" },
-    blueGreyDark: {light: "background-color :#C2C9FD"},
-    blueGreyLight: { light: "background-color :#EDF1fc" },
-    black: { light: "background-color :#353535" },
-    blackDeep: { light: "background-color :#1D1D1D" },
-    blackLight: { light: "background-color :#FEFEFE" },
-  });
+  white: { light: "color :#FFFFFF" },
+  whiteDark: { light: "color :#F9F9FB" },
+  whiteDarker: { light: "color :#F6F7FB" },
+  blue: { light: "color :#5569FF" },
+  blueLight: { light: "color :#4355FE" },
+  blueGrey: { light: "color :#EEF0FC" },
+  blueGreyDark: { light: "color :#C2C9FD" },
+  blueGreyLight: { light: "color :#EDF1fc" },
+  black: { light: "color :#353535" },
+  blackDeep: { light: "color :#1D1D1D" },
+  blackLight: { light: "color :#FEFEFE" }
+});
 
-  const Colors = styled.div`
-    ${color};
-    ${bgColor};
+export const bgColor = theme.variants("mode", "bgColor", {
+  white: { light: "background-color :#FFFFFF" },
+  whiteDark: { light: "background-color :#F9F9FB" },
+  whiteDarker: { light: "background-color :#F6F7FB" },
+  blue: { light: "background-color :#5569FF" },
+  blueLight: { light: "background-color :#4355FE" },
+  blueGrey: { light: "background-color :#EEF0FC" },
+  blueGreyDark: { light: "background-color :#C2C9FD" },
+  blueGreyLight: { light: "background-color :#EDF1fc" },
+  black: { light: "background-color :#353535" },
+  blackDeep: { light: "background-color :#1D1D1D" },
+  blackLight: { light: "background-color :#FEFEFE" }
+});
 
-  `;
+const Colors = styled.div`
+  ${color};
+  ${bgColor};
+  height: 30px;
+  width: 20px;
+`;
 
-  Colors.propTypes = {
-    variant: PropTypes.oneOf(["white", "blue", "black"])
-  };
+Colors.propTypes = {
+  variant: PropTypes.oneOf(["white", "blue", "black"])
+};
 
-  export default Colors
+Colors.defaultProps = {
+  color: "blue"
+}
+
+export default Colors;
--- src/stories/colors.stories.js
+++ src/stories/colors.stories.js
@@ -2,23 +2,23 @@ import React from "react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withKnobs } from "@storybook/addon-knobs";
 
-import {Color} from "../components/colors";
+import Colors from "../components/colors";
 import { ThemeProvider } from "styled-components";
 const theme = {
   mode: "light"
 };
 
 export default {
-  title: "Color",
-  component: Color,
+  title: "color",
+  component: Colors,
   decorators: [withA11y, withKnobs]
 };
 
 
 export const bigColor = () => (
   <ThemeProvider theme={theme}>
-    <Color color="white" >
-    </Color>
+    <Colors color="blue" bigColor="black">
+    </Colors>
   </ThemeProvider>
 );
 
