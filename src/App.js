e5218116d837e56c75b4a3d649fc53b6060cf82e
kingsley okpara <kingsconsult001@gmail.com>
2020-02-23 02:01:11 +0100

    created the navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Helmet } from "react-helmet";
-import NavLinks from "./components/links"
-import Button from "./components/buttons"
-import Typography from "./components/typography"
-import Circles from "./components/circles"
-import Line from "./components/lines"
-import Card from "./components/cards"
-import { ThemeProvider } from "styled-components"
-const theme={
-  mode:"light"
-}
+import NavLinks from "./components/links";
+import Button from "./components/buttons";
+import Typography from "./components/typography";
+import Circles from "./components/circles";
+import Line from "./components/lines";
+import Card from "./components/cards";
+import NavBar from "./molecules/navbar";
+import { ThemeProvider } from "styled-components";
+const theme = {
+  mode: "light"
+};
 
 function App() {
   return (
@@ -18,30 +19,10 @@ function App() {
       <div className="App">
         <Helmet
           bodyAttributes={{
-            style: "background : #Fff 0% 0% no-repeat padding-box"
+            style: "background : #Ff3f 0% 0% no-repeat padding-box"
           }}
         />
-        <div>
-          <h1>Vertical Card</h1>
-          <div>
-            <Card cardlayout="vertical" bgColor="white"></Card>
-          </div>
-        </div>
-        <div>
-          <h1>horizontal Card</h1>
-
-          <Card cardlayout="horizontal" bgColor="white"></Card>
-        </div>
-        <div>
-          <h1>horizontal Long</h1>
-
-          <Card cardlayout="horizontalLong" bgColor="white"></Card>
-        </div>
-        <div>
-          <h1>horizontal Banner</h1>
-
-          <Card cardlayout="horizontalBanner" bgColor="white"></Card>
-        </div>
+        <NavBar></NavBar>
       </div>
     </ThemeProvider>
   );
