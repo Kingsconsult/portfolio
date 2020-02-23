0627bd0ff997f6014f0c1c89deb2defb47c12e18
kingsley okpara <kingsconsult001@gmail.com>
2020-02-23 12:23:53 +0100

    create a component, I called creative works, that showcase my creative works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Helmet } from "react-helmet";
-import NavLinks from "./components/links"
-import Button from "./components/buttons"
-import Typography from "./components/typography"
-import Circles from "./components/circles"
-import Line from "./components/lines"
-import Card from "./components/cards"
+import CreativeWork from "./molecules/creativeworks"
 import { ThemeProvider } from "styled-components"
 const theme={
   mode:"light"
@@ -21,27 +16,7 @@ function App() {
             style: "background : #Fff 0% 0% no-repeat padding-box"
           }}
         />
-        <div>
-          <h1>Vertical Card</h1>
-          <div>
-            <Card cardlayout="vertical" bgColor="white"></Card>
-          </div>
-        </div>
-        <div>
-          <h1>horizontal Card</h1>
-
-          <Card cardlayout="horizontal" bgColor="white"></Card>
-        </div>
-        <div>
-          <h1>horizontal Long</h1>
-
-          <Card cardlayout="horizontalLong" bgColor="white"></Card>
-        </div>
-        <div>
-          <h1>horizontal Banner</h1>
-
-          <Card cardlayout="horizontalBanner" bgColor="white"></Card>
-        </div>
+<CreativeWork></CreativeWork>
       </div>
     </ThemeProvider>
   );
